b2706c9cc73bfc150ee65718d817dd8f983b9060
Lehndorff <lehndorff@evergreenecon.com>
2023-03-25 18:11:03 -0700

    Start of final ML project

---
+++ Final Project/basic_model.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(caret)
+library(pROC)
+library(MLmetrics)
+library(randomForest)
+
+tourney_data <- read_csv("tourney_data.csv") %>% 
+  mutate(rank=log2(rank)) %>% # converts 1,2,4,8,16, etc to 0,1,2,3,4
+  select(-Season,-class,-`Coach(es)`,-`NCAA Tournament`) %>% #either not helpful, or duplicative
+  filter(!is.na(Pts_for)) %>% #removes 1 case
+  mutate(across("AP Pre":"AP Final",function(x){ifelse(is.na(x),"Not 25",paste("Top",ceiling(x/5)*5))})) %>% #data cleaning
+  mutate(across(Conf_W:Conf_Pct,function(x){ifelse(is.na(x),0,x)})) #%>% #fill missing info
+
+skimr::skim(tourney_data)
+
+tourney_data2<-tourney_data %>%
+  fastDummies::dummy_cols(remove_selected_columns = T) %>% #simple dummy cols
+  arrange(tourn_year) %>% mutate(lag_rank=lag(rank)) %>% mutate(lag_rank=ifelse(is.na(lag_rank),6,lag_rank)) %>% #how did the team do last year
+  mutate(rank=as.numeric(rank<=4))# <=4 = Did team make the sweet 16 (one of the final 2^4 teams)
+table(tourney_data2$rank)
+
+# ctrl <- trainControl(method = "cv", number = 5)
+set.seed(504) 
+
+# tourney_index <- createDataPartition(tourney_data2$rank, p = 0.80, list = FALSE)
+# train <- tourney_data2[ tourney_index, ]
+# test <- tourney_data2[-tourney_index, ]
+
+train<-tourney_data2 %>% 
+  group_by(rank) %>% 
+  sample_frac(.8)
+test<-tourney_data2 %>% 
+  anti_join(train)
+
+control <- trainControl(method = "boot", number = 1)
+fit_rf <- train(rank ~ .,
+             data = train,
+             method = "rf",
+             trControl = control)
+
+plot(varImp(fit_rf),top=10)
+
+pred <- predict(fit_rf, newdata=test)
+
+data.frame(test=test$rank,pred=pred) %>% 
+  ggplot()+
+  geom_density(aes(x=pred,fill=factor(test),group=test),alpha=.5)+
+  theme(legend.position = "bottom")+
+  labs(x="Prediction",fill="Actual")
+
+#A function for determining the best threshold for cutoff (needed for regression not categorization)
+best_thresh<-function(x){
+  pred2<-as.numeric(pred>x)
+  output<-as.numeric(confusionMatrix(factor(pred2),factor(test$rank))$overall[2])
+  return(output)
+}
+
+cutoff<-data.frame(thresh=seq(0,1,.01)) %>% 
+  mutate(best=sapply(thresh,best_thresh)) %>% 
+  filter(best==max(best)) %>% 
+  pull(thresh)
+pred2<-as.numeric(pred>cutoff)
+
+confusionMatrix(factor(pred2),factor(test$rank))
+
+
+
---
+++ Final Project/data_acquisition.R
@@ -0,0 +1,93 @@
+library(rvest)
+library(purrr)
+library(tidyverse)
+
+csv_skip<-function(x){
+  x %>% 
+    write.csv("file.csv",row.names = F)
+  x=read_csv("file.csv",skip = 1)
+  return(x)
+}
+
+# Define the URL of the website
+url <- "https://www.sports-reference.com/cbb/schools/"
+
+# Read the HTML code of the website
+webpage <- read_html(url)
+
+# Extract the table on the webpage
+table <- html_nodes(webpage, "table")
+
+# Extract the links from the table
+links <- html_nodes(table, "a") %>% html_attr("href")
+links<-links[grepl("/men/",links)]
+
+# Create a list to store the data from each page
+data_list <- list()
+
+# Loop through each link and extract the data from the corresponding page
+for (link in links[201:490]) {
+  Sys.sleep(runif(1)+3)
+  
+  # Construct the URL of the page
+  print(link)
+  page_url <- paste0("https://www.sports-reference.com", link)
+  
+  # Use read_html() function to extract the HTML code of the page
+  page_html <- read_html(page_url)
+  
+  # html_text(html_nodes(page_html,"span")[9])
+  
+  # Use html_table() function to extract the data from the page
+  data <- page_html %>% html_table() %>% csv_skip() %>% mutate(team=html_text(html_nodes(page_html,"span")[9])) %>% 
+    mutate(across(everything(),as.character))
+  
+  # Store the data in the list
+  data_list[[link]] <- data
+}
+
+# Combine the data from all pages into a single data frame
+all_data <- bind_rows(data_list)
+all_data<-all_data %>% 
+  rename(Ovr_W=W...4,Ovr_L=L...5,Ovr_Pct=`W-L%...6`,
+         Conf_W=W...7,Conf_L=L...8,Conf_Pct=`W-L%...9`,
+         Pts_for=PTS...12,Pts_against=PTS...13) %>% 
+  mutate(across(Ovr_W:`AP Final`,as.numeric)) %>% 
+  filter(!is.na(Ovr_W)) %>% 
+  mutate(tourn_year=as.numeric(str_extract(Season,"^[0-9]{4}"))+1)
+
+all_data %>% 
+  filter(!is.na(`NCAA Tournament`)) %>% 
+  group_by(tourn_year,`NCAA Tournament`) %>% 
+  summarise(n=n()) %>% 
+  View()
+
+table(all_data$`NCAA Tournament`[all_data$tourn_year>=1985&all_data$tourn_year<2023])
+
+
+
+selected_data<-all_data %>% 
+  filter(!is.na(`NCAA Tournament`)&between(tourn_year,1985,2022)) %>% 
+  filter(`NCAA Tournament`!="Playing NCAA Tournament First Round")
+
+clean_data<-selected_data %>% 
+  mutate(class=`NCAA Tournament`) %>% 
+  mutate(class=ifelse(`NCAA Tournament`=="Lost NCAA Tournament First Four"|`NCAA Tournament`=="Lost NCAA Tournament Opening Round","Lost NCAA Tournament First Round",class)) %>%
+  mutate(class=ifelse(`NCAA Tournament`=="Lost NCAA Tournament First Round"&between(tourn_year,2011,2015),"Lost NCAA Tournament Second Round",class)) %>% 
+  # group_by(`NCAA Tournament`,tourn_year) %>% 
+  # summarise(n=n()) %>% 
+  mutate(rank=case_when(
+    grepl("Won",`NCAA Tournament`)~1,
+    grepl("National Final",`NCAA Tournament`)~2,
+    grepl("National Semifinal",`NCAA Tournament`)~4,
+    grepl("Regional Final",`NCAA Tournament`)~8,
+    grepl(" Regional Semifinal",`NCAA Tournament`)~16,
+    grepl("Third Round",`NCAA Tournament`)&between(tourn_year,2011,2015)~32,
+    grepl("Second Round",`NCAA Tournament`)&!between(tourn_year,2011,2015)~32,
+    TRUE~64
+  )) %>% 
+  select(-Rk)
+
+table(log2(clean_data$rank))
+write.csv(clean_data,file="Final Project/tourney_data.csv",row.names = F)
+      
